c935cc40539204d7118d987b9319916eed996f51
Alice Won <alicewon4@gmail.com>
2019-12-11 17:56:47 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,64 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect "/articles"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  #FORM
+  get '/articles/new' do
+    erb :new
+  end 
+
+  #EDIT
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #CREATE---
+  post '/articles' do
+    #place code that extracts the form data from the params and uses it to create a new instance of your model class
+   
+    @article = Article.create(params[:article])
+    # (:title => params[:title], :content => params[:content])
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  
+
+
+  #showpage needs extra parameter (id)
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :show
+  end
+
+  #READ---
+
+  #UPDATE---
+
+  #forms that delete and edit need to be submitted via (html) POST // controller = delete requests
+
+  #DELETE---
+  
+  delete '/articles/:id' do
+    #already in show page, no need for erb, same with update, create (post), only need for forms (create and update first part)
+
+    @article = Article.find(params[:id])
+    @article.delete
+    redirect '/articles'
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  #active record brings this all in for CREATE
+  
+  # attr_reader = :title, :content
+  # @@all = []
+
+  # def initialize(title, content)
+  #   @title = title
+  #   @content = content
+  #   @@all << self
+  # end
+
+  # def self.all
+  #   @@all
+  # end
 end
